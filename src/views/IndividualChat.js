6a577bbc0226b1dabc907014fe1caffa934816f1
adeabreu <0deabreubarbara@gmail.com>
2024-02-16 20:57:33 -0500

    Estructura css de individual chat

--- src/views/IndividualChat.js
+++ src/views/IndividualChat.js
@@ -6,6 +6,32 @@ export const IndividualChat = () => {
   const main = document.createElement("main");
   main.innerHTML = `
     <h2>Chat individual</h2>
+    <div class="chat-container">
+   <div class="passport-wrapper">
+    <div class="passport-background">
+      <div class="upper-flap">
+        <div class="passport-icon"></div>
+      </div>
+      <div class="lower-flap">
+        <div class="flap-header"></div>
+        <div class="flap-body">
+          <div class="passport-image"></div>
+          <div class="passport-info"></div>
+        </div>
+        <div class="flap-footer"></div>
+      </div>
+    </div>
+  </div>
+  <div class="chat-wrapper">
+    <div class="chat-background">
+      <div class="chat-content">
+        <div class="chat-header"></div>
+        <div class="chat-body"></div>
+        <div class="chat-footer"></div>
+      </div>
+    </div>
+  </div>
+</div>
   `;
 
   container.append(Header(), main, Footer());
