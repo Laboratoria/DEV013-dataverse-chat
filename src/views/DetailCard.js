1b1fd064f576abfb101847cec2dbf187b26c3c69
Melany Ventura <melanymarisolventura@gmail.com>
2024-02-21 16:03:35 -0400

    Agregue la funcion Detail card

--- src/views/DetailCard.js
+++ src/views/DetailCard.js
@@ -1,17 +1,47 @@
-//import { Header } from "../Components/Header"
-
+import { Header } from "../Components/Header.js";
+import { Card } from "../Components/Card.js";
 
 export const DetailCard = (props) => {
-    
-    console.log(props);
-  const detailCard = document.createElement('div');
-  detailCard.innerText = `
-    <div>
-        <img src="" class="">
-        <h3> Hola </h3>
+  console.log(props);
+  // esto contiene la vista completa de la tarjeta seleccionada
+  const viewDetailCard = document.createElement('section');
+  // Llamando al DOM que sera para el detalle de la tarjeta
+  const detailCard = document.createElement("main");
+  const card = document.createElement("div");
+  const buttonsContainer = document.createElement("div");
+  // Agregando clases
+  viewDetailCard.setAttribute("class","viewDetailCard");
+  detailCard.setAttribute("class", "detailCard");
+  card.setAttribute("class", "card");
+  buttonsContainer.setAttribute("class", "buttonsContainer");
 
-
-    </div>
+  //agregando html a los botones
+  buttonsContainer.innerHTML=`
+  <button class="button-principal">
+    <img src="src/images/home.png" class="logo-home">
+    Pagina principal
+  </button>
+  <button class="button-chat">
+  <img src="src/images/chat.png" class="logo-chat-single">
+  Chatear
+  </button>
   `;
+  return buttonsContainer;
+  
+  //realizamos el contenedor
+  viewDetailCard.appendChild(Header());
+  viewDetailCard.appendChild(DetailCard);
+  detailCard.appendChild(card);
+  detailCard.appendChild(buttonsContainer);
+
+  
+  // detailCard.innerText = `
+  //   <div>
+  //       <img src="" class="">
+  //       <h3> Hola </h3>
+
+
+  //   </div>
+  // `;
   return detailCard;
 }
