c3a4dabe43731b856c5abd4db18d8a37fb4efc08
Etelbina <etelbinacanedo@gmail.com>
2024-02-22 18:53:10 -0600

    HTML not found component

--- src/views/notfound.js
+++ src/views/notfound.js
@@ -1,15 +1,41 @@
 import { navigateTo } from "../router.js";
 
-export default function NotFound(props) {
+export default function NotFound() {
   const viewNotFound = document.createElement("div");
-  viewNotFound.textContent = "404";
+
+  viewNotFound.innerHTML += `
+    <h1>404</h1>
+    <p>Oh no!!</p>
+    <p>
+      Lamentamos que hayas<br />
+      llegado al final del jardín
+    </p>
+    <p>Para ver más plantas</p>
+    <!--<button id="homeButton">Ir a inicio</button>-->
+  `;
 
   const homeButton = document.createElement("button");
   viewNotFound.appendChild(homeButton);
-  const textButton = document.createTextNode("Go back home");
+  const textButton = document.createTextNode("Go not found");
   homeButton.appendChild(textButton);
 
-  homeButton.addEventListener("click", () => navigateTo("/Home", props));
+  homeButton.addEventListener("click", () => navigateTo("/Home"));
 
   return viewNotFound;
 }
+
+// function goHome() {
+//   const button = document.getElementById("homeButton");
+//   button.addEventListener("click", () => {
+//     navigateTo("/Home");
+//   });
+// }
+
+// const creatingNotFound = () => {
+//   const viewNotFound = document.createElement("div");
+//   viewNotFound.innerHTML += `
+//   <h1>404</h1>
+//   <button>Go back home</button>
+//   `;
+//   return viewNotFound;
+// };
