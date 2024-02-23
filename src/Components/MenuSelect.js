7fe94bed241015c331f47d0d1f14b12ba39546e3
Antone Gonzales <es.angelika@gmail.com>
2024-02-22 21:55:43 -0500

    cambie el nombre de las propiedades en ingles al componente MenuSelect.js

--- src/Components/MenuSelect.js
+++ src/Components/MenuSelect.js
@@ -4,9 +4,9 @@ export const MenuSelect = () => {
   selects.innerHTML = `
     <div class= menu>
     <!--Seccion genero-->
-    <div class="menuGenero">
-      <label for="Genero1">Genero: </label>
-      <select id="Genero1" name="genero" data-testid="select-filter">
+    <div class="Genre-menu">
+      <label for="Genre1">Genero: </label>
+      <select id="Genre1" name="genre" data-testid="select-filter">
         <option value="a" selected disabled>seleccionar</option>
         <option value="Romance">Romance</option>
         <option value="Drama">Drama</option>
@@ -17,18 +17,18 @@ export const MenuSelect = () => {
       </select>
     </div>
     <!--Seccion ordenar A-Z-->
-    <div class="menuOrdenar">
-      <label for="Ordenar1">Ordenar: </label>
-      <select id="Ordenar1" name="ordenar" data-testid="select-sort">
+    <div class="Order-menu">
+      <label for="Order1">Ordenar: </label>
+      <select id="Order1" name="ordenar" data-testid="select-sort">
         <option value="a" selected disabled>seleccionar</option>
         <option value="asc">A - Z</option>
         <option value="desc">Z - A</option>
       </select>
     </div>
     <!--Seccion Estadisticas-->
-    <div class="menuEstadistica">
-      <label for="Estadisticas1">Estadística: </label>
-      <button id="Estadisticas1">Número de episodios</button>
+    <div class="statistics-menu">
+      <label for="statistics1">Estadística: </label>
+      <button id="statistics1">Número de episodios</button>
       <div class="content-modal" id="modal">
         <div class="modal-body">
           <div class="modal-header">
@@ -47,7 +47,7 @@ export const MenuSelect = () => {
       </div>
     </div>
     <!--Boton Limpiar filtros-->
-    <div class="menuLimpiar">
+    <div class="Menu-clean">
       <button name="button-clear" data-testid="button-clear">
         <img class="logoLimpiar" src="images/filter.png" />
         Limpiar filtros</button>
