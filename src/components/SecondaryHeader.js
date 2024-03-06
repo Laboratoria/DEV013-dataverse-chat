270641a14df4bc724fa97d338bd57950ded2b9fc
adeabreu <0deabreubarbara@gmail.com>
2024-03-05 19:18:27 -0500

    nuevos cambios

--- src/components/SecondaryHeader.js
+++ src/components/SecondaryHeader.js
@@ -1,15 +1,14 @@
-// import { navigateTo } from '../router.js';
+import { Button } from "../components/Button.js";
 
 export const SecondaryHeader = () => {
   const header = document.createElement("header");
+  header.className = "secundary-header"
   header.innerHTML = `
   <div class="header-title">
     <img src="./images/Logo.png" alt="Logo" />
       <h1>WikiNook</h1>
   </div>
-    <div>
-    <button class="btn-header">??</button>
-    </div>
-    </nav>
     `;
-}
+  header.appendChild(Button());
+  return header;
+};
