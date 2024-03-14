ffc4ba3b90ed25956cb627ccda0fd00c28479688
Antone Gonzales <es.angelika@gmail.com>
2024-03-13 19:16:27 -0500

    modifique el header para que sea responsive en Header.js y style.css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,13 @@
-import { navigateTo } from "../router.js";
-
 export const Header = (data) => {
   const header = document.createElement('header');
   header.setAttribute("class", "header");
   header.innerHTML = `
-    <  
-  <img src="images/news.gif" class= "logo-header">
+  <div id= "container-left">  
+    <img src="images/news.gif" class= "logo-header">
     <h1 class= "title-kdrama">K-DRAMA <span class= "title-verse">VERSE</span></h1>
-    
+  </div>
+  
+  <div "container-righ">
     <button class="button-group-chat">
     <img src="images/group.png" class="logo-group-chat">
     Chat Grupal
@@ -16,13 +16,7 @@ export const Header = (data) => {
     <button class="button-appi-key">
     Api Key
     </button>
+  </div>
   `;
-
- 
-  // Boton apikey a donde nos drecciona con navigateTo
-  // const buttonAppiKey = header.querySelector(".button-appi-key");
-  // buttonAppiKey.addEventListener("click", () => {
-  //   navigateTo("Modal ", {})
-  // })
   return header;
 }
