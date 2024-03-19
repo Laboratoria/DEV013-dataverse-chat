55ba024dbdf47accef379233bc62a10bf3a57077
Antone Gonzales <es.angelika@gmail.com>
2024-03-18 22:51:39 -0500

    agregue el test unitario para el componente Footer.js en footer.spec.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
 export const Footer = () => {
   const footerElement = document.createElement("footer");
   footerElement.setAttribute("class", "footer");
-  footerElement.innerText = "Copyright @ 2024 Antone Gonzales & Melany Ventura";
+  footerElement.innerHTML = `<p>Copyright @ 2024 Antone Gonzales & Melany Ventura</p>`;
   return footerElement;
 };
---
+++ test/footer.spec.js
@@ -0,0 +1,28 @@
+import { Footer } from "../src/Components/Footer.js";
+//agrupamos las pruebas con describe para el componente footer
+describe("Footer component", () => {
+  it("El Componente footer existe", () => {
+    const footer = Footer();
+    // aqui afirmamos que footer es verdadero 
+    // asegurarnos de que exista y no sea false, null, undefined
+    expect(footer).toBeTruthy();
+  });
+
+  it("El contenido del footer no está vacío.", () => {
+    const footer = Footer();
+    // parrafo 
+    const paragraph = footer.querySelector("p");
+    //afirmamos que el texto dentro del elemento <p> (paragraph.textContent) 
+    //no debe ser igual a una cadena vacía.
+    expect(paragraph.textContent).not.toEqual("");
+  });
+
+  it("El footer muestra el contenido correcto", () => {
+    const footer = Footer();
+    // aqui verificamos que la classe de footer sea footer
+    expect(footer.className).toBe("footer");
+    //verificamos que el texto dentro del elemento <p> dentro del Footer 
+    //sea igual al texto que mostramos.
+    expect(footer.querySelector("p").textContent).toBe("Copyright @ 2024 Antone Gonzales & Melany Ventura")
+  });
+});
